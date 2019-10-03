b8dfcb33d69b3177c0005af4878d889364030539
usman18 <uk32971@gmail.com>
2019-10-04 01:01:14 +0530

    -> Gender selected by default

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Gender _selectedGender;
+  Gender _selectedGender = Gender.Male;
 
   int age = 21;
   int weight = 60;
